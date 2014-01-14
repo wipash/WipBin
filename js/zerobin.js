96b632b71456f8fba4fba7b7ecd7b315326126c6
Sean McGrath <sean@mcgrath.net.nz>
2014-01-14 00:20:55 +0000

    Fixed syntax highlighting being applied twice

--- js/zerobin.js
+++ js/zerobin.js
@@ -178,7 +178,8 @@ function applySyntaxColoring()
         // highlight.js expects code to be surrounded by <pre><code>
         $('div#cleartext').html('<pre><code>'+ $('div#cleartext').html()+'</code></pre>');
     }
-    hljs.highlightBlock(document.getElementById('cleartext'));
+    $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+    //hljs.highlightBlock(document.getElementById('cleartext'));
     $('div#cleartext').css('padding','0'); // Remove white padding around code box.
 }
 
