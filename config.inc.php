997b18a7144d1770b98701f9ea93b14b8bb558b0
wipash <wispernet@gmail.com>
2014-03-06 10:00:27 +1300

    Added Google Analytics options

--- config.inc.php
+++ config.inc.php
@@ -72,4 +72,14 @@ $cfg["showHash"] = true;
  */
 $cfg["enableDiscussionSystem"] = false;
 
+
+/*
+ * Google analytics options, possible privacy issues with enabling this
+ */
+$cfg["enableGoogleAnalytics"] = false;
+$cfg["googleAnalyticsTrackingID"] = "XX-99999999-9";
+$cfg["googleAnalyticsTrackingSite"] = "site.com";
+
+
+
 ?>
